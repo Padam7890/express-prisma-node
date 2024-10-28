87adafdb5d67c4ceab4832d564985b2b8d1167af
Padam7890 <padamthapamoho@gmail.com>
2024-10-28 21:11:53 +0545

    added prisma

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 
 const express = require('express');
+ 
+const {PrismaClient} = require("@prisma/client");
+
+const prisma = new PrismaClient();
+
 
 const app = express();
 
@@ -10,6 +15,28 @@ app.get("/", (req,res)=> {
         version: "1.0.0"
     });
 })
+
+app.post("/", async (req,res)=>{
+    const user = {
+        name:"padam",
+        email:"padam@gmail.com",
+        address:"123 Main Street"
+    }
+    const saveData  = await prisma.user.create({
+        data:{
+            name:user.name ,
+            email:user.email,
+            addresses: user.address,
+        }
+    })
+    res.json({
+        message: "Data saved successfully",
+        data:saveData
+    });
+
+
+})
+
 
 
 
