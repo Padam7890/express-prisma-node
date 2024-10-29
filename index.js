e49b6955d9fb92800875ff72649454e622dc50c7
Padam7890 <padamthapamoho@gmail.com>
2024-10-29 10:21:34 +0545

    http method added

--- index.js
+++ index.js
@@ -1,52 +1,44 @@
+const express = require("express");
 
-const express = require('express');
- 
-const {PrismaClient} = require("@prisma/client");
+const { PrismaClient } = require("@prisma/client");
 
 const prisma = new PrismaClient();
 
-
 const app = express();
 
-
-app.get("/", (req,res)=> {
-    res.json({
-        message: "Welcome to the API",
-        version: "1.0.0"
-    });
-})
-
-app.post("/", async (req,res)=>{
-    const user = {
-        name:"padam",
-        email:"padam@gmail.com",
-        address:"123 Main Street"
-    }
-    const saveData  = await prisma.user.create({
-        data:{
-            name:user.name ,
-            email:user.email,
-            addresses: user.address,
-        }
-    })
-    res.json({
-        message: "Data saved successfully",
-        data:saveData
-    });
+app.use(express.json());
 
 
-})
+app.get("/", async (req, res) => {
+  //provider(prisma, tablename(user), method()
+  const getUser = await prisma.user.findMany();
+  return res.status(200).json({
+    message: "user successfully fetched",
+    data: getUser,
+  });
+});
 
+app.post("/", async (req, res) => {
+ const {name,email, addresses} = req.body;  
 
+  const saveData = await prisma.user.create({
+    data: {
+      name,
+      email,
+      addresses,
+    },
+  });
 
+  res.json({
+    message: "Data saved successfully",
+    data: saveData,
+  });
+
+  
+});
 
 const port = 3000;
 
 app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}`);
-})
-
-
-
-
-
+  console.log(`Server running at http://localhost:${port}`);
+});
