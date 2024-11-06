48386897593e12f8351b6da51945f0f4a7b46610
Padam7890 <padamthapamoho@gmail.com>
2024-11-06 12:14:47 +0545

    crud added

--- index.js
+++ index.js
@@ -1,14 +1,39 @@
 const express = require("express");
+const router = express.Router();
 
-const { PrismaClient } = require("@prisma/client");
+const { PrismaClient,Prisma } = require("@prisma/client");
+const { PrismaClientValidationError, PrismaClientUnknownRequestError } = require("@prisma/client/runtime/library");
 
 const prisma = new PrismaClient();
 
+prisma
+  .$connect()
+  .then((result) => {
+    console.log("Connected to database");
+  })
+  .catch((err) => {
+    console.log("Error connecting to database", err);
+    process.exit(1);
+  });
 
 const app = express();
 
 app.use(express.json());
 
+
+
+
+
+
+
+
+app.get("/", function (req, res) {
+  res.send("Hello World!");
+});
+
+app.get("/padam", function (req, res) {
+  res.send("Hello Padam!");
+});
 
 app.get("/", async (req, res) => {
   //provider(prisma, tablename(user), method()
@@ -19,9 +44,12 @@ app.get("/", async (req, res) => {
   });
 });
 
+
+
+
 app.post("/user", async (req, res) => {
- const {name,email, addresses} = req.body;  
- console.log(req.body)
+  const { name, email, addresses } = req.body;
+  console.log(req.body);
   const saveData = await prisma.user.create({
     data: {
       name,
@@ -36,30 +64,27 @@ app.post("/user", async (req, res) => {
   });
 });
 
-app.put("/user/:id", async (req, res)=> {
-  const {id}  = req.params; //kunchaima update garne 
-  const {name,email, addresses} = req.body;    //k vau update garne
+app.put("/user/:id", async (req, res) => {
+  const { id } = req.params; //kunchaima update garne
+  const { name, email, addresses } = req.body; //k vau update garne
 
-  //updateb database 
+  //updateb database
 
   const updateDb = await prisma.user.update({
     where: {
-      id:parseInt(id)
+      id: parseInt(id),
     },
-    data:{
-      name:name,
-      email:email,
-      addresses:addresses
-    }
-  })
+    data: {
+      name: name,
+      email: email,
+      addresses: addresses,
+    },
+  });
   return res.status(200).json({
-    data:updateDb,
-    message:"user updated successfully",
-  })
-
-
-})
-
+    data: updateDb,
+    message: "user updated successfully",
+  });
+});
 
 const port = 3000;
 
