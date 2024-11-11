0fc04a506e49219385654280a4dcb442b102f11d
Padam7890 <padamthapamoho@gmail.com>
2024-11-11 13:24:02 +0545

    user create method added

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,50 +1,66 @@
+const prisma = require("../config/prisma");
+const  bcrypt = require('bcryptjs');
+
 function getUser(req, res) {
   res.send("hye i am a user");
 
 
 }
 
-function createUser(req, res) {
+async function createUser(req, res) {
 
+  ///get data from body
   const { name, addresses, password, email } = req.body;
 
-  // Check if any of the required fields are missing
+  // Check if any of the required fields are missing (validation)
   if (!name || !addresses || !password || !email) {
     return res.status(400).json({
       message: "Please fill in all required fields: name, addresses, password, and email.",
     });
   }
 
-  /// Assignment: pisma le user table ma gayera email chai xaki xaina khojna parne
-  
-  example : Prisma.user.findmany({
-    where:{
-      email:email
+  //check emil if already exist or not (check if unique)
+
+  const isExist =  prisma.user.findUnique({
+    where: {
+      email
     }
   })
 
-  
+  if(!isExist){
+    return res.status(400).json({
+      message: "Email already exist",
+    });
+  }
 
 
+  //hash password
+  const genSalt = await bcrypt.genSalt(10);
+  const hashpassword = await bcrypt.hash(password, genSalt)
 
 
+  //create user 
 
+  const createUser = await prisma.user.create({
+    data:{
+      name:name,
+      email:email,
+      addresses:addresses,
+      password:hashpassword
+    }
+  })
 
-
-
-
-
-
-
-  
-
-
-}
+  return res.json({
+    message: "User created successfully",
+    user: createUser,
+  })
+ }
 
 function homepage(req, res) {
      res.send("Hey i am a homepage");
 }
 
+console.log('hey')
 
 
 
--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const express = require("express");
 const app = express();
 const userRouter =  require("./routes/user.route")
 
+app.use(express.json());
+
 app.use('/', userRouter )
 
 const port = 3000;
