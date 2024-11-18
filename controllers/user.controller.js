078718d23ec0ca7d691d2d49e1f6c6c07c2fc7d3
Padam7890 <padamthapamoho@gmail.com>
2024-11-18 13:10:51 +0545

    feat:login successully implemented

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -2,23 +2,27 @@ const prisma = require("../config/prisma");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
-function getUser(req, res) {
-  res.send("hye i am a user");
 
-  //database bata data haru get garne
+// const body = {
+//   "name":"padam"
+// }
 
-  // example:prisma.user.findMany();
 
-  // res.json({
-  //   messgae:"data fetch"
-  //   data: example.data
-  // })
+
+async function getUser(req, res) {
+  const getUser = await prisma.user.findMany();
+  return res.status(200).json({
+    message:"User Found",
+    data: getUser
+
+  })
+
 }
 
 async function createUser(req, res) {
-  ///get data from body
+  ///get data from bo
   const { name, addresses, password, email } = req.body;
-  console.log(req.body);
+  console.log(req);
 
   // Check if any of the required fields are missing (validation)
   if (!name || !addresses || !password || !email) {
@@ -80,22 +84,48 @@ function homepage(req, res) {
   res.send("Hey i am a homepage");
 }
 
+async function login() {
+ 
+  const {email,password} = req.body;
+  const checkEmail = await prisma.user.findUnique({
+    where:{
+      email,
+    }
+  })
+  if(!checkEmail){
+    return res.status(401).json({
+      message: "Email not found",
+    })
+  }
 
-function login() {
-  //req.body garera data line
-  //validation haru garne 
-  //email already xaki xaina 
-  //password match hunu paryo
-  //token generate karna paryo
-  //  return.res.status(200).json({
-  //   tokem:token
-  //  })
-  
+  const isPassword = bcrypt.compare(password, checkEmail.password);
+  if(!isPassword){
+    return res.status(401).json({
+      message: "Password not match",
+    })
+  }
+  const tokenGenerate = jwt.sign(
+    {
+      name: createUser.name,
+      email: createUser.email,
+    },
+    "padam",
+    {
+      expiresIn: "1h",
+    }
+)
+
+return res.status(200).json({
+  message:"Use Logged In Succesfully",
+  token:tokenGenerate
+})
+
 }
 
 console.log("hello");
 
 module.exports = {
+  login,
   getUser,
   createUser,
   homepage,
--- routes/user.route.js
+++ routes/user.route.js
@@ -1,10 +1,11 @@
 const express = require('express');
-const {getUser, createUser, homepage} = require('../controllers/user.controller');
+const {getUser, createUser, homepage, login} = require('../controllers/user.controller');
 
 const router  = express.Router();
 
 //router below
 router.get('/user', getUser)
+router.post("/login", login )
 router.get("/", homepage )
 router.post('/user', createUser)
 
